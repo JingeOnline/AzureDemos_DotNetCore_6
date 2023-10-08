a009a546c4f3ddd5046ba9b1a8ac9e9ba162cdba
Jinge Zhang <jinge_windows@hotmail.com>
2023-10-09 00:04:45 +1100

    Add project files.

---
+++ ServiceBus_Receiver/Program.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using ServiceBus_SharedLibrary;
+using System.Text.Json;
+
+namespace ServiceBus_Receiver
+{
+    public class Program
+    {
+        static ServiceBusClient client;
+        static ServiceBusProcessor processor;
+        static string queueName = "jingetestqueue";
+
+        static async Task Main(string[] args)
+        {
+            string connectString = getJsonConfig("AzureServiceBusConnectionString");
+            var clientOptions = new ServiceBusClientOptions()
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets  //使用443端口
+            };
+            client = new ServiceBusClient(connectString, clientOptions);
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+                Console.WriteLine("Message received: (Press any key to exit)");
+                //开始接收消息
+                await processor.StartProcessingAsync();
+                Console.ReadKey();
+                //结束接收消息
+                await processor.StopProcessingAsync();
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+
+        //处理接收到的消息
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            MessageModel? messageModel = JsonSerializer.Deserialize<MessageModel>(body);
+            Console.WriteLine();
+            Console.WriteLine($"Sender: {messageModel?.Sender}");
+            Console.WriteLine($"Time: {messageModel?.SendTime}");
+            Console.WriteLine($"Content: {messageModel?.Content}");
+            Console.WriteLine();
+
+            //需要显式的调用消息接收完成的方法，之后该消息会从queue中删除
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        //处理接收消息过程中的异常
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        //获取本地文件夹中储存的密钥，防止密钥泄露到GitHub上。
+        public static string getJsonConfig(string key)
+        {
+            string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string filePath = Path.Combine(userFolder, "OneDrive\\脚本代码\\AccountSecrets.json");
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(filePath);
+            IConfiguration config = builder.Build();
+            var section = config.GetSection(key);
+            return section.Value;
+        }
+    }
+}
---
+++ ServiceBus_Sender/Program.cs
@@ -0,0 +1,107 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using ServiceBus_SharedLibrary;
+using System.Text;
+using System.Text.Json;
+
+namespace ServiceBus_Sender
+{
+    public class Program
+    {
+        static ServiceBusClient client;
+        static ServiceBusSender sender;
+        static string queueName = "jingetestqueue";
+
+        static async Task Main(string[] args)
+        {
+            string connectString = getJsonConfig("AzureServiceBusConnectionString");
+            var clientOptions = new ServiceBusClientOptions
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets  //使用443端口
+            };
+            client = new ServiceBusClient(connectString,clientOptions);
+            sender = client.CreateSender(queueName);
+
+            try
+            {
+                while (true)
+                {
+                    MessageModel messageModel = new MessageModel();
+                    Console.WriteLine("Please input username:");
+                    messageModel.Sender = Console.ReadLine();
+                    Console.WriteLine("Please input message");
+                    messageModel.Content = Console.ReadLine();
+                    messageModel.SendTime = DateTime.Now;
+                    await SendMessageAsync(messageModel);
+                    Console.WriteLine("Send Succeed. Continue? Y/N");
+                    if (String.Compare(Console.ReadLine(), "N", true) == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+            
+        }
+
+        public static async Task SendMessageAsync<T>(T serviceBusMessage)
+        {
+            string jsonText = JsonSerializer.Serialize(serviceBusMessage);
+            ServiceBusMessage message = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonText));
+            await sender.SendMessageAsync(message);
+
+        }
+
+        #region 下面的代码使用了Microsoft.Azure.ServiceBus包，该包已经弃用
+        //static QueueClient queueclient;
+        //static string queueName = "jingetestqueue";
+
+        //static async Task Main(string[] args)
+        //{
+        //    string connectString = getJsonConfig("AzureServiceBusConnectionString");
+        //    queueclient = new QueueClient(connectString, queueName);
+
+        //    while(true)
+        //    {
+        //        MessageModel messageModel = new MessageModel();
+        //        Console.WriteLine("Please input username:");
+        //        messageModel.Sender = Console.ReadLine();
+        //        Console.WriteLine("Please input message");
+        //        messageModel.Content = Console.ReadLine();
+        //        messageModel.SendTime = DateTime.Now;
+        //        await SendMessageAsync(messageModel);
+        //        Console.WriteLine("Send Succeed. Continue? Y/N");
+        //        if(String.Compare(Console.ReadLine(),"N",true)==0)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    await queueclient.CloseAsync();
+        //}
+
+        //public static async Task SendMessageAsync<T>(T serviceBusMessage)
+        //{
+        //    string jsonText=JsonSerializer.Serialize(serviceBusMessage);
+        //    Message message = new Message(Encoding.UTF8.GetBytes(jsonText));
+        //    await queueclient.SendAsync(message);
+
+        //}
+        #endregion
+
+        //获取本地文件夹中储存的密钥，防止密钥泄露到GitHub上。
+        public static string getJsonConfig(string key)
+        {
+            string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string filePath = Path.Combine(userFolder, "OneDrive\\脚本代码\\AccountSecrets.json");
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(filePath);
+            IConfiguration config = builder.Build();
+            var section = config.GetSection(key);
+            return section.Value;
+        }
+    }
+}
